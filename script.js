f701c89fa87b74e0f129b2703288ba2031c799e5
o-lat <omarl1933@gmail.com>
2017-05-10 10:27:22 +0100

    added getBMI() function and required textbox var.

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
---
+// BMI JS File
+
+var weight = document.getElementById('textbox1');
+var height = document.getElementById('textbox2');
+
+function getBMI() {
+  if (weight.value == '' || height.value == '') {
+    alert('Both fields must be populated.');
+  } else {
+    alert('Your BMI is ', weight.value / (height.value * height.value));
+  }
+}
