54ba184dfb9fbb149465b4e421cbf334705bd327
o-lat <omarl1933@gmail.com>
2017-05-10 10:37:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,6 @@ function getBMI() {
   if (weight.value == '' || height.value == '') {
     alert('Both fields must be populated.');
   } else {
-    alert('Your BMI is ', weight.value / (height.value * height.value));
+    alert('Your BMI is ', weight.value / height.value * height.value);
   }
 }
