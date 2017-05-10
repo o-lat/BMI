cd4f0928cd92d07242b34fa938dcfac2a96b44f3
o-lat <omarl1933@gmail.com>
2017-05-10 11:44:13 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 function getBMI() {
     var weight = parseInt(document.getElementById("textbox1").value);
     var height = parseInt(document.getElementById("textbox2").value);
-	var outputBMI = document.getElementById('ouput_bmi');
-	var outputMeasure = document.getElementById('ouput_measure');
+	var outputBMI = document.getElementById('output_bmi');
+	var outputMeasure = document.getElementById('output_measure');
 	var bmiText = document.getElementById('bmi_text');
 	var measureText = document.getElementById('measure_text');
 	var BMI = (weight / (height * height));
@@ -13,32 +13,32 @@ function getBMI() {
 		alert('Please enter a valid BMI.');
 		console.log(BMI);
 	} else if(BMI <= 18.4) {
-		outputBMI.innerHTML(BMI);
-		outputMeasure.innerHTML('underweight');
+		outputBMI.innerHTML = BMI;
+		outputMeasure.innerHTML = 'underweight';
 		bmiText.style.display = 'block';
 		measureText.style.display = 'block';
 		console.log(BMI);
 	} else if(BMI >= 30) {
-		outputBMI.innerHTML(BMI);
-		outputMeasure.innerHTML('normal');
+		outputBMI.innerHTML = BMI;
+		outputMeasure.innerHTML = 'normal';
 		bmiText.style.display = 'block';
 		measureText.style.display = 'block';
 		console.log(BMI);
 	} else if(BMI >= 25) {
-		outputBMI.innerHTML(BMI);
-		outputMeasure.innerHTML('overweight');
+		outputBMI.innerHTML = BMI;
+		outputMeasure.innerHTML = 'overweight';
 		bmiText.style.display = 'block';
 		measureText.style.display = 'block';
 		console.log(BMI);
 	} else if(BMI >= 18.5) {
-		outputBMI.innerHTML(BMI);
-		outputMeasure.innerHTML('obese');
+		outputBMI.innerHTML = BMI;
+		outputMeasure.innerHTML = 'obese';
 		bmiText.style.display = 'block';
 		measureText.style.display = 'block';
 		console.log(BMI);
 	} else {
-		outputBMI.innerHTML(BMI);
-		outputMeasure.innerHTML('Error');
+		outputBMI.innerHTML = BMI;
+		outputMeasure.innerHTML = 'Error';
 		bmiText.style.display = 'block';
 		measureText.style.display = 'block';
 		console.log(BMI);
