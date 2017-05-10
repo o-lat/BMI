a050d0bfae8f92c0a51472c9d16936db74ffaed0
o-lat <omarl1933@gmail.com>
2017-05-10 14:45:40 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // JS File
 
 function getBMI() {
-	var weight = parseInt(document.getElementById("textbox1").value);
-	var height = parseInt(document.getElementById("textbox2").value);
+	var weight = document.getElementById("textbox1").value;
+	var height = document.getElementById("textbox2").value;
 	var outputBMI = document.getElementById('output_bmi');
 	var outputMeasure = document.getElementById('output_measure');
 	var bmiText = document.getElementById('bmi_text');
@@ -12,28 +12,28 @@ function getBMI() {
 		alert('Please enter a valid BMI.');
 		console.log(BMI);
 	} else if(BMI <= 18.4) {
-		outputBMI.innerHTML = BMI;
+		outputBMI.innerHTML = Math.round(BMI);
 		outputMeasure.innerHTML = 'Underweight';
 		showOutput();
 		console.log(BMI);
-	} else if(BMI >= 30) {
-		outputBMI.innerHTML = BMI;
+	} else if(BMI <= 24.9) {
+		outputBMI.innerHTML = Math.round(BMI);
 		outputMeasure.innerHTML = 'Normal';
 		showOutput();
 		console.log(BMI);
-	} else if(BMI >= 25) {
-		outputBMI.innerHTML = BMI;
+	} else if(BMI <= 29.9) {
+		outputBMI.innerHTML = Math.round(BMI);
 		outputBMI.style.display = 'block';
 		outputMeasure.innerHTML = 'Overweight';
 		showOutput();
 		console.log(BMI);
-	} else if(BMI >= 18.5) {
-		outputBMI.innerHTML = BMI;
+	} else if(BMI >= 30) {
+		outputBMI.innerHTML = Math.round(BMI);
 		outputMeasure.innerHTML = 'Obese';
 		showOutput();
 		console.log(BMI);
 	} else {
-		outputBMI.innerHTML = BMI;
+		outputBMI.innerHTML = Math.round(BMI);
 		outputMeasure.innerHTML = 'Error';
 		showOutput();
 		console.log(BMI);
